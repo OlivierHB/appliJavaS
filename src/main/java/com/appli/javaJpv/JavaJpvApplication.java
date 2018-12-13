685aa66c3c66b830ffe9dca3bf12bea76b119a75
IPI <1olivier.bouda@gmail.com>
2018-12-13 09:23:57 +0100

    Premier commit

---
+++ src/main/java/com/appli/javaJpv/JavaJpvApplication.java
@@ -0,0 +1,14 @@
+package com.appli.javaJpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJpvApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/appli/javaJpv/JavaJpvApplicationTests.java
@@ -0,0 +1,17 @@
+package com.appli.javaJpv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaJpvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
